60a4227bff25facf901f7466ff8d0fb62602bded
Igor <garkol@bk.ru>
2022-08-05 21:47:58 +0300

    task one

---
+++ HomeWork05_01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив, заполненный случайными положительными числами. Напишите программу, которая покажет 
+// количество четных чисел в массиве.
+
+Console.WriteLine("Задайте размер массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int count = 0;
+for (int i = 0; i < n; i++)
+    {
+    array[i] = new Random().Next(1, 1000);
+    if (array[i] % 2 == 0)
+        count++;
+    }
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine("Данный массив содержит " + count + "четных чисел.");
